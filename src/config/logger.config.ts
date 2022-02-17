ef20b7d34572e0405983181ff7c080de6f4fc07b
agnoam <agnoam.aghai.ooo.123@gmail.com>
2022-02-17 22:32:47 +0200

    Changed code of winston logger

--- src/config/logger.config.ts
+++ src/config/logger.config.ts
@@ -1,22 +1,54 @@
-import winston from 'winston';
+import winston, { Logger } from 'winston';
 import debugFormat from 'winston-format-debug';
+import { ElasticsearchTransport } from 'winston-elasticsearch';
+import { ElasticSerachConfig } from './elasticsearch.config';
 
 console.log('import logger.config');
 
 export module LoggerConfig {
-    const logger = winston.createLogger({
-        level: 'info',
-        format: winston.format.json(),
-        defaultMeta: { service: 'user-service' },
-        transports: [
-            new winston.transports.Console({
-                format: winston.format.combine(
-                    winston.format.colorize({ message: true }),
-                    debugFormat({
-                        colorizeMessage: false // Already colored by `winston.format.colorize`.
-                    })
-                )
-            }),
-        ]
-    });
+    export let logger: Logger;
+    
+    export const initialize = (configs?: ILoggerInitProps): void => {
+        if (!logger) {
+            logger = winston.createLogger({
+                level: 'info',
+                format: winston.format.json(),
+                defaultMeta: configs?.defaultMetadata || { service: 'non-set-service-name', version: '-1.0.0' },
+                transports: [
+                    // process.env.NODE_ENV !== 'production' ?
+                        new winston.transports.Console({
+                            format: winston.format.combine(
+                                winston.format.timestamp(),
+                                winston.format.colorize({ message: true }),
+                                // winston.format.printf((info) => {
+
+                                // })
+                            )
+                        })
+                    // : undefined,
+                    // new ElasticsearchTransport({
+                    //     indexPrefix: `${configs.serviceName}-logs`,
+                    //     client: ElasticSerachConfig.elasticClient
+                    // })
+                ]
+            });
+
+            console.log('Logger has been created');
+            // overrideConsole();
+        }
+    }
+
+    const overrideConsole = () => {
+        if (!logger) throw 'Can not override console without an initialized logger';
+
+        console.log = (message?: any, ...optionalParams: any[]) => logger.info(message, ...optionalParams);
+        console.debug = (message?: any, ...optionalParams: any[]) => logger.debug(message, ...optionalParams);
+        console.warn = (message?: any, ...optionalParams: any[]) => logger.warn(message, ...optionalParams);
+        console.error = (message?: any, ...optionalParams: any[]) => logger.error(message, ...optionalParams);
+    }
+}
+
+export interface ILoggerInitProps {
+    serviceName?: string;
+    defaultMetadata?: Object;
 }
